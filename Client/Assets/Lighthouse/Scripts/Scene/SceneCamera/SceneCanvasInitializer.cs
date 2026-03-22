dd6f4ed6be48985715026af085285e82f06cb22e
Lise <archeleeds@gmail.com>
2026-03-22 14:05:13 +0900

    fix

--- Client/Assets/Lighthouse/Scripts/Scene/SceneCamera/SceneCanvasInitializer.cs
+++ Client/Assets/Lighthouse/Scripts/Scene/SceneCamera/SceneCanvasInitializer.cs
@@ -6,10 +6,19 @@ namespace Lighthouse.Scene.SceneCamera
     {
         [SerializeField] Canvas[] sceneCanvasList;
 
+        void Awake()
+        {
+            foreach (var canvas in sceneCanvasList)
+            {
+                canvas.enabled = false;
+            }
+        }
+
         public void Initialize(ISceneCamera canvasCamera)
         {
             foreach (var canvas in sceneCanvasList)
             {
+                canvas.enabled = true;
                 canvas.renderMode = RenderMode.ScreenSpaceCamera;
                 canvas.worldCamera = canvasCamera.GetCamera();
             }
